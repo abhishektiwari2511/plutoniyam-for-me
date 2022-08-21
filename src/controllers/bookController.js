4d5a3a478da80c287b7fe8c91960f0c2f54630fd
abhishek <abhishek251119@gmail.com>
2022-08-21 14:44:26 +0530

    this is new assingment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,47 @@
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
 
 const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+     let book = req.body
+     let author= book.author
+     let publisher= book.publisher
+     let author_id= await authorModel.findById(author).select({_id:1})
+     let publisher_id= await publisherModel.findById(publisher).select({_id:1})
+     if(!author){
+        return res.send("author detail is required");
+     } if(!author_id){
+        return res.send("the author_id is not correct");
+     }if (!publisher){
+        return res.send("publisher details is required");
+     }if (!publisher_id){
+        return res.send("the publisher_id is not correct")
+     }
+     let bookCreated= await bookModel.create(book)
+     return res.send(bookCreated)
+   
+
 }
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
-}
+
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+    let specificBook = await bookModel.find().populate('author').populate("publisher")
     res.send({data: specificBook})
 
 }
+const allbook =async function(req,res){
+   let allbook= await bookModel.updateMany(
+      {publisherName :{$in :["peguin","harpercollins"]}},
+      {$set :{isHardcover:false}},
+      {new:true}
+   )
+   res.send({msg: allbook})
+}
+
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+
+module.exports.allbook=allbook
